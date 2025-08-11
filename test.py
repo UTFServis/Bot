9200fc2588f1f15f0f955051e86fe570bb3004e9
UTFServis <abwalfdlrfyy92@gmail.com>
2025-08-11 08:40:18 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+
+TOKEN = "8289509583:AAEMZIIFtJfw-hQDtc6PxHIVOje5UfkYzdw"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("Instagram", callback_data='send_instagram')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('سلام! دکمه اینستا رو بزن:', reply_markup=reply_markup)
+
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    if query.data == 'send_instagram':
+        with open("C:/Users/TUF Gaming/Desktop/index.html", "rb") as f:
+            await query.message.reply_document(f)
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(button_handler))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
